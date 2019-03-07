6dead3b4deb357c00c53c84def8ea317095f84ac
Fahad Ahmed <fahmed7x@walgreenco.net>
2019-03-07 12:50:15 -0600

    refactoring template into components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import ContactMe from './Components/ContactMe';
+import Footer from './Components/Footer.js';
+import Portfolio from './Components/Portfolio';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <ContactMe />
+        <Footer />
       </div>
     );
   }
